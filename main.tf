03b8f812b9f3c35d59bc60ae26ba14cb788864fb
Chin Hiroshi (陳 浩) <chin-hiro@aeonpeople.biz>
2023-10-18 19:49:21 +0900

    Remove cloud block

---
+++ main.tf
@@ -0,0 +1,43 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+/*
+  cloud {
+    organization = "squall-tutorial-org"
+
+    workspaces {
+      name    = "learn-terraform-cloud"
+      project = "learn-terraform-azure"
+    }
+  }
+*/
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "japaneast"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "japaneast"
+  resource_group_name = azurerm_resource_group.rg.name
+}
